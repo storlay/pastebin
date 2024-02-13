9e1c34810d7d0a8bdfdd39a7bd6889d09b58d236
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-02-13 11:26:12 -0300

    Add datetime field

--- text/forms.py
+++ text/forms.py
@@ -1,12 +1,30 @@
+from datetime import datetime
+
 from django import forms
-from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 
 from text.models import Text
 
 
-class InputTextForm(ModelForm):
-    url_hash = forms.CharField(label='Введите текст', widget=forms.Textarea(attrs={'class': 'form-control'}))
+class InputTextForm(forms.ModelForm):
+
+    def clean_datetime_of_deletion(self):
+        data = self.cleaned_data['datetime_of_deletion']
+        if datetime.now() > data:
+            raise ValidationError('Дата и время уничтожения сообщения не могут быть меньше текущих')
+        return data
 
     class Meta:
         model = Text
-        fields = ('url_hash',)
+        fields = '__all__'
+        required = ('url_hash',)
+        labels = {
+            'url_hash': 'Текст',
+            'is_temporary': 'Временное сообщение',
+            'datetime_of_deletion': 'Дата и время уничтожения сообщения'
+        }
+        widgets = {
+            'url_hash': forms.Textarea(attrs={'class': 'form-control', 'rows': 7}),
+            'is_temporary': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'datetime_of_deletion': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'})
+        }
--- text/models.py
+++ text/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Text(models.Model):
     url_hash = models.TextField()
+    is_temporary = models.BooleanField(default=False)
+    datetime_of_deletion = models.DateTimeField(blank=True, null=True)
 
     def get_absolute_url(self):
         from django.urls import reverse
