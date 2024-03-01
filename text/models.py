33a7b67d6bc3a4f9766fa88c46d47f6a84343e22
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-03-01 16:00:33 -0300

    Fix "drive_id" length in Text model

--- text/models.py
+++ text/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Text(models.Model):
     uuid_url = models.UUIDField(unique=True, editable=False)
-    drive_id = models.CharField(max_length=33)
+    drive_id = models.CharField(max_length=44)
     is_temporary = models.BooleanField(default=False)
     datetime_of_deletion = models.DateTimeField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', blank=True, null=True)
