be67e86be58c873c2553d5db6a0176d278c58637
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-03-05 13:52:36 -0300

    Move static files

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
