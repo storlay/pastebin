ac03ff3cdc38b723e6bdf23cd7484e6ad92e186e
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2024-03-15 17:29:04 -0300

    Add Google authentication

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -33,6 +33,7 @@ CSRF_COOKIE_SECURE = True
 # Authentication
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthenticationBackend',
 ]
@@ -52,6 +53,10 @@ SOCIAL_AUTH_PIPELINE = [
 
 SOCIAL_AUTH_GITHUB_KEY = os.environ.get('SOCIAL_AUTH_GITHUB_KEY')
 SOCIAL_AUTH_GITHUB_SECRET = os.environ.get('SOCIAL_AUTH_GITHUB_SECRET')
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+
 
 INSTALLED_APPS = [
     'admin_interface',
